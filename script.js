18e5fec8dbd21a82e5336d38f087435815d79853
UMR_Work <97790149+UMRwork@users.noreply.github.com>
2022-01-25 20:07:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function GitHub(){
+  location.replace("https://github.com/UMRwork")
+}
+function Twitter(){
+  location.replace("https://twitter.com/UMR_Hungry")
+}
+function YouTube(){
+  location.replace("https://www.youtube.com/channel/UCnrQt6Snk7a3CM5UmlYqsdQ")
+}
+
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
